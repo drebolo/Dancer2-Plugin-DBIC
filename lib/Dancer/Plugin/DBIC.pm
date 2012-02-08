985fbe2299b6fd99852d0b2b119456bedea0d437
Naveed Massjouni <naveedm9@gmail.com>
2012-02-07 23:23:43 -0500

    Updating POD documentation

--- lib/Dancer/Plugin/DBIC.pm
+++ lib/Dancer/Plugin/DBIC.pm
@@ -13,94 +13,95 @@ DBIx::Class::Schema::Loader->naming('v7');
 
 =head1 SYNOPSIS
 
-    # Dancer Code File
     use Dancer;
-    use Dancer::Plugin::DBIC;
+    use Dancer::Plugin::DBIC 'schema';
 
-    get '/profile/:id' => sub {
-        my $user = schema->resultset('Users')->find(params->{id});
-        # or explicitly ask for a schema by name:
-        $user = schema('foo')->resultset('Users')->find(params->{id});
-        template user_profile => { user => $user };
+    get '/users/:id' => sub {
+        my $user = schema->resultset('User')->find(param 'id');
+        template user_profile => {
+            user => $user
+        };
     };
 
     dance;
 
-    # Dancer config file (config.yml) or environments/*.yml
-    plugins:
-      DBIC:
-        dsn:  "dbi:SQLite:dbname=./foo.db"
-        schema_class: "My::Schema"
-
-If you're using multiple database connections or schemas, you can also provide
-them with names, then pass the corresponding name to C<schema> to get the
-appropriate one:
-
-    plugins:
-      DBIC:
-        foo:
-          dsn: "dbi:SQLite:dbname=foo.db"
-        bar:
-          dsn: "dbi:SQLite:dbname=bar.db"
-
-See below for more detailed configuration examples.
-
-
 =head1 DESCRIPTION
 
-This plugin provides an easy way to obtain L<DBIx::Class::Schema> instances
-via the provided C<schema> keyword, which it automatically exports.
-
-You just need to put your database connection details in your L<Dancer> 
-configuration file, generate schema classes by hand or using L<dbicdump> (or
-allow the plugin to automatically take care of that by using
-L<DBIx::Class::Schema::Loader> - but this isn't recommended for production use),
-and you're ready to go.
-
+This plugin makes it very easy to create L<Dancer> applications that interface
+with databases.
+It automatically exports the keyword C<schema> which returns a
+L<DBIx::Class::Schema> object.
+You just need to configure your database connection information.
+For performance, schema objects are cached in memory
+and are lazy loaded the first time they are accessed.
 
 =head1 CONFIGURATION
 
-Connection details will be grabbed from your L<Dancer> config file.
-For example: 
+Configuration can be done in your L<Dancer> config file.
+This is a minimal example:
 
     plugins:
       DBIC:
+        default:
+          dsn: dbi:SQLite:dbname=some.db
+
+In this example, there are 2 databases configured named default and foo.
+
+    plugins:
+      DBIC:
+        default:
+          dsn: dbi:SQLite:dbname=some.db
+          schema_class: My::Schema
         foo:
-          dsn: dbi:SQLite:dbname=./foo.db
-        bar:
-          schema_class: Foo::Bar
-          dsn:  dbi:mysql:db_foo
-          user: root
+          dsn:  dbi:mysql:foo
+          schema_class: Foo::Schema
+          user: bob
           pass: secret
           options:
             RaiseError: 1
             PrintError: 1
 
-Each schema configuration *must* have a dsn option.
+Each database configured must have a dsn option.
 The dsn option should be the L<DBI> driver connection string.
 All other options are optional.
 
 If a schema_class option is not provided, then L<DBIx::Class::Schema::Loader>
-will be used to auto load the schema based on the dsn value - but see below for
-caveats.
+will be used to dynamically load the schema based on the dsn value.
+This is for convenience only and should not be used in production.
+See L</"SCHEMA GENERATION"> below for caveats.
 
-The schema_class option, if provided, should be a proper Perl package name that
-Dancer::Plugin::DBIC will use as a DBIx::Class::Schema class.
-Optionally, a database configuation may have user, pass and options paramters
-as described in the documentation for connect() in L<DBI>.
+The schema_class option, should be a proper Perl package name that
+Dancer::Plugin::DBIC will use as a L<DBIx::Class::Schema> class.
+Optionally, a database configuation may have user, pass, and options parameters
+as described in the documentation for C<connect()> in L<DBI>.
 
-    # Note: You can also declare your connection information with the
-    # following syntax:
+You may also declare your connection information in the following format
+(which may look more familiar to DBIC users):
+
     plugins:
       DBIC:
-        foo:
+        default:
           connect_info:
-            - dbi:mysql:db_foo
-            - root
+            - dbi:mysql:foo
+            - bob
             - secret
             -
               RaiseError: 1
               PrintError: 1
+
+=head1 USAGE
+
+This plugin provides just the keyword C<schema> which
+returns a L<DBIx::Class::Schema> object ready for you to use.
+If you have configured only one database, then you can call C<schema> with
+no arguments:
+
+    my $user = schema->resultset('User')->find('bob');
+
+If you have configured multiple databases,
+then you must give C<schema()> the name of the database as an argument:
+
+    my $user = schema('foo')->resultset('User')->find('bob');
 
 =head1 SCHEMA GENERATION
 
